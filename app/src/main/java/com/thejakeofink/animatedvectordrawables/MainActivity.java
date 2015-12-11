df8a60e7e44071628be57d3f6e7f8a06ab160eab
Jake <thejakeofink@gmail.com>
2015-12-10 18:03:41 -0700

    updated up_caret to use a resource for its stroke width. Also checked for instance of Animatable.

--- app/src/main/java/com/thejakeofink/animatedvectordrawables/MainActivity.java
+++ app/src/main/java/com/thejakeofink/animatedvectordrawables/MainActivity.java
@@ -1,9 +1,9 @@
 package com.thejakeofink.animatedvectordrawables;
 
-import android.graphics.drawable.AnimatedVectorDrawable;
+import android.graphics.drawable.Animatable;
 import android.graphics.drawable.Drawable;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
 
@@ -28,8 +28,8 @@ public class MainActivity extends AppCompatActivity {
                 Drawable d = getDirectionalDrawable();
 
                 myImageView.setImageDrawable(d);
-                if (d instanceof AnimatedVectorDrawable) {
-                    ((AnimatedVectorDrawable) d).start();
+                if (d instanceof Animatable) {
+                    ((Animatable) d).start();
                 }
 
                 isUp = !isUp;
